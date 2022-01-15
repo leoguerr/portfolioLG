2f2f7f59bd844dd7dac51fe7a17e3dc47bde94cc
Leo Guerrero <laoguerr@gmail.com>
2022-01-15 16:48:36 -0600

    adding asset images and updating HTML

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+  //Collapsable
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+        
+  //carousel 
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
