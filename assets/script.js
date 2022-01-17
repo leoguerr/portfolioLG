fe327024c6ed7d8b1b2f7d8eb27f58ad14d7bd53
Leo Guerrero <laoguerr@gmail.com>
2022-01-16 18:17:38 -0600

    updating HTML and javascript files

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,27 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.carousel').carousel();
   });
+  // nav bar links 
+  var navbar = document.getElementById("navbar");
+  var about = document.getElementById("about");
+  var aboutC =document.getElementById("aboutC");
+  var works = document.getElementById("works");
+  var contact = document.getElementById("contact");
+  var contactC = document.getElementById("contactC");
+  var workC = document.getElementById("workC");
+  var list = document.querySelector("#aboutC")
+
+  about.addEventListener("click", function() {
+    document.querySelector("#aboutC").className += "active";
+  $('.collapsible').collapsible();
+  });
+
+  works.addEventListener("click", function() {
+    document.querySelector("#workC").className += "active";
+  $('.collapsible').collapsible();
+  });
+
+  contact.addEventListener("click", function() {
+    document.querySelector("#contactC").className += "active";
+  $('.collapsible').collapsible();
+  });
